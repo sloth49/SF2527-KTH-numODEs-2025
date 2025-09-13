6d2350f05b66296cd800cf113e732180a2f81479
sloth49 <158371384+sloth49@users.noreply.github.com>
2025-09-13 23:08:21 +0200

    part 4a: added Laplacian and RHS vector assembly function. Not tested

--- CE1/part_4a.py
+++ CE1/part_4a.py
@@ -13,48 +13,139 @@ import scipy.sparse.linalg as spla
 from scipy.linalg import solve
 
 
-def create_grid(Lx: float, Ly: float, N: int):             
+def create_grid(Lx: float, Ly: float, N: int):
+    """
+    Creates a grid of coordinates in a rectangular domain
+    No of nodes in (x, y) direction: (N+1, M+1)
+    First and last nodes are placed on the boundary of the domain
+    The step size is taken as equal in each direction
+
+    Parameters
+        Lx: length in x direction
+        Ly: length in y direction
+        N: number of division in the x direction
+
+    Returns:
+        X: array of x coordinates
+        Y: array of y coordinates
+        h: step size
+        M: No of steps in y direction (ca)
+    """
     x = np.linspace(start=0, stop=Lx, num=N+1)
     h = x[1] - x[0]
+
     M = int(Ly / h)
     y = np.linspace(start=0, stop=Ly, num=M+1)
+
     X, Y = np.meshgrid(x, y)
+
     return X, Y, h, M
 
 
-def Sn(n: int, h: float) -> sp.csr_matrix:
+def Sn_DN(n: int, h: float) -> sp.csr_matrix:
     """
     One-dimensional finite-difference derivative matrix 
-    of size (n-1) x (n-1) for second derivative:
+    of size (n+1) x (n+1) solving:
+        -u'' = f
 
-    For n=5, for example:
- 
-    h^2 * Sn =
-         2    -1      0     0     0
-        -1     2     -1     0     0
-         0    -1      2    -1     0
-         0     0     -1     2    -1
-         0     0      0    -1     2
+    Boundary conditions:
+      Start point: Dirichlet
+      End point: Neumann homogeneous 
 
-    Lecture notes reference: handout #7 page 22
+    For n=4, for example: 
+
+               | -1    0                 |
+               |  0    2   -1            |
+    h^2 * Sn = |      -1    2   -1       |
+               |           -1    2   -1  |
+               |                -2    2  |
     """
 
-    diag_main = np.full(n-1, 2)
-    diag_off = np.full(n-2, -1)
-    A = sp.diags([diag_main, diag_off, diag_off],
-                 [0, 1, -1], shape=(n-1, n-1), format='csr')
+    diag_main = np.full(shape=n+1, fill_value=2)
+    diag_main[0] = -1
+
+    diag_lower = np.full(shape=n, fill_value=-1)
+    diag_lower[0] = 0
+    diag_lower[-1] = -2
+
+    diag_upper = diag_lower.copy()
+    diag_upper[-1] = -1
+
+    A = sp.diags(
+        diagonals=[diag_lower, diag_main, diag_upper],
+        offsets=[-1, 0, 1], shape=(n+1, n+1), format='lil')
     
-    return A / (h**2)
+    return (A.tocsr()) / (h**2)
+
+
+def Sn_NN(n: int, h: float) -> sp.csr_matrix:
+    """
+    One-dimensional finite-difference derivative matrix 
+    of size (n+1) x (n+1) solving:
+        -u'' = f
+
+    Boundary conditions:
+      Start point: Neumann homogeneous
+      End point: Neumann homogeneous 
+
+    For n=4, for example:
+
+               |  2   -2                 |
+               | -1    2   -1            |
+    h^2 * Sn = |      -1    2   -1       |
+               |           -1    2   -1  |
+               |                -2    2  |
+    """
+
+    diag_main = np.full(shape=n+1, fill_value=2)
+
+    diag_lower = np.full(shape=n, fill_value=-1)
+    diag_lower[-1] = -2
+
+    diag_upper = diag_lower.copy()
+    diag_upper[0] = -2
+
+    A = sp.diags(
+        diagonals=[diag_lower, diag_main, diag_upper],
+        offsets=[-1, 0, 1], shape=(n+1, n+1), format='lil')
+    
+    return (A.tocsr()) / (h**2)
 
 
 def laplacian_2d(N: int, M: int, h: float) -> sp.csr_matrix:
-    S_N = Sn(n=N, h=h)
-    S_M = Sn(n=M, h=h)    
-    Lp = (
-        sp.kron(sp.eye(M-1, format="csr"), S_N, format="csr")
-        + sp.kron(S_M, sp.eye(N-1, format="csr"), format="csr")
-    )
-    return Lp
+    """
+    Implements formula on slides #7 page 22
+    """
+    S_N = Sn_NN(n=N, h=h)
+    S_M = Sn_DN(n=M, h=h)
+    I_M = sp.eye(M+1, format="csr")
+    I_N = sp.eye(N+1, format="csr")
+
+    return (sp.kron(I_M, S_N, format="csr") + sp.kron(S_M, I_N, format="csr"))
+
+
+def f_grid(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    Forcing function
+    """
+    return np.full_like(a=x, fill_value=2)
+
+
+def get_rhs(X: np.ndarray, Y: np.ndarray, T_ext: float, h: float) -> np.ndarray:
+    """
+    docstring
+    """
+
+    rhs = f_grid(X, Y).ravel(order='C')
+
+    n = X.shape[1] - 1
+    m = Y.shape[0] - 1
+    dirichlet_BC_term = T_ext / h**2
+    rhs[:(n+1)] = dirichlet_BC_term
+    rhs[(n+1):2*(n+1)] += dirichlet_BC_term
+
+    return rhs
+    
 
 
 # Define the problem parameters
@@ -68,5 +159,11 @@ X_PROBE, Y_PROBE = 6.0, 2.0
 # Grid generation
 X, Y, h, M = create_grid(Lx, Ly, N)
 A = laplacian_2d(N=N, M=M, h=h)
+# print(A.toarray())
+rhs = get_rhs(X=X, Y=Y, T_ext=T_EXT, h=h)
+T = spla.spsolve(A, rhs)
+# pass
 
-# Forcing function
+# print(rhs.shape)
+# for i in range(N+1):
+#     print(rhs[i])
